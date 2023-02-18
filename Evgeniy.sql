caa33e6ddf66db6e72f39bcf6b4c8990d61767ab
Evgeniy610 <125756348+Evgeniy610@users.noreply.github.com>
2023-02-18 19:56:37 +0300

    init: add file

---
+++ Evgeniy.sql
@@ -0,0 +1,4 @@
+insert into jobs values ('Логист', 800, 1200);
+insert into jobs values ('Водитель', 1500, 1800);
+insert into jobs values ('Программист', 1800, 2200);
+insert into jobs values ('Специалист по кибербезопастности', 2000, 2400);
